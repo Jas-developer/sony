1e8663604c3f988e7bab36a27c51c93fe94a1b3b
Jason Evaristo <devop.jas@gmail.com>
2023-05-24 14:41:06 +0800

    fixed responsibility

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,6 @@
 import featured from "../assets/b.png";
 import Lense from "../assets/LenseFeatured.png";
+
 const Featured = () => {
   return (
     <div className="bg-light mt-2">
@@ -7,16 +8,16 @@ const Featured = () => {
         <h1 className="text-light">Sony</h1>
       </div>
       <div className="row">
-        <div className="col-md-6 mt-2 bg-success ">
+        <div className="col-md-6 mt-2">
           <div className="card bg-dark" id="featured-card">
             <div className="card-body">
               <div className="row">
-                <div className="col-sm-7 text-center ">
-                  <img src={featured} alt="" className="p-5" />
+                <div className="col-md-7 text-center">
+                  <img src={featured} alt="" className="img-fluid" />
                 </div>
-                <div className="col-sm-5 text-success p-5 ">
+                <div className="col-md-5">
                   <h5 className="card-title">Featured Post</h5>
-                  <p className="card-text">
+                  <p className="card-text text-success">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
@@ -31,16 +32,16 @@ const Featured = () => {
             </div>
           </div>
         </div>
-        <div className="col-md-6 mt-2 bg-success ">
-          <div className="card " id="featured-card">
+        <div className="col-md-6 mt-2">
+          <div className="card" id="featured-card">
             <div className="card-body">
               <div className="row">
-                <div className="col-sm-7 text-center ">
-                  <img src={Lense} alt="" className="p-5" />
+                <div className="col-md-7 text-center">
+                  <img src={Lense} alt="" className="img-fluid" />
                 </div>
-                <div className="col-sm-5 text-success p-5">
+                <div className="col-md-5">
                   <h5 className="card-title">Featured Post</h5>
-                  <p className="card-text">
+                  <p className="card-text text-success">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
