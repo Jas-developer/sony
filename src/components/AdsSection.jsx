94def9145aeccc9b5e44136df613eef2ef57060b
Jason Evaristo <devop.jas@gmail.com>
2023-05-24 18:41:04 +0800

    added 2 more row

--- src/components/AdsSection.jsx
+++ src/components/AdsSection.jsx
@@ -15,11 +15,27 @@ const AdsSection = () => {
             ></iframe>
           </div>
         </div>
-        <div className="col-md-6 text-light p-5">
-          Our new Sony ZV-E1 is an interchangeable-lens vlog camera with a
-          high-performance 35mm full-frame image sensor. Positioned at the top
-          of Sony’s Vlog camera series, it gives creative vloggers who want to
-          deliver the highest quality content.
+        <div className="col-md-6  p-5">
+          <div className="row">
+            <div className="col-md-12  text-success">
+              {" "}
+              Our new Sony ZV-E1 is an interchangeable-lens vlog camera with a
+              high-performance 35mm full-frame image sensor. Positioned at the
+              top of Sony’s Vlog camera series, it gives creative vloggers who
+              want to deliver the highest quality content.
+            </div>
+            <div className="row p-5">
+              <div className="col-md-6 bg-warning">
+                <h1 className="text-center">First row</h1>
+              </div>
+              <div className="col-md-6 p-5 bg-primary">
+                <h1 className="text-center  ">
+                  Second row
+                  <br />
+                </h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
